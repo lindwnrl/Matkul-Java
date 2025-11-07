030ef7430497663a04d64deec19a06fa491c16cd
lindwnrl <lindwnrl@gmail.com>
2025-11-07 13:26:32 +0700

    Modifikasi percobaan 4

--- Jobsheet9/SearchNilai17.java
+++ Jobsheet9/SearchNilai17.java
@@ -1,18 +1,33 @@
+import java.util.Scanner;
+
 public class SearchNilai17 {
     public static void main(String[] args) {
-        int nilai[] ={80,85,78,96,90,82,86};
-        int key = 90;
+        Scanner sc = new Scanner(System.in);
+        int key;
         int hasil = 0;
+        int mahasiswa;
         
+        System.out.print("Masukkan jumlah mahasiswa yang akan diinput : ");
+        mahasiswa = sc.nextInt();
+        
+        int nilai[] = new int[mahasiswa];
+        for (int i = 0;i <mahasiswa;i++){
+            System.out.print("Masukkan nilai mahasiswa : ");
+            nilai[i]= sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari : ");
+        key = sc.nextInt();
 
         for (int i = 0;i <nilai.length;i++){
             if (key == nilai [i]){
-                hasil = i;
+                hasil = i + 1;
+                 System.out.println("Nilai "+key+" ketemu di mahasiswa ke-"+hasil);
+                break;
+            }else {
+                System.out.println("Nilai yang dicari tidak ditemukan.");
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
-        System.out.println();
+        sc.close();
     }
 }
