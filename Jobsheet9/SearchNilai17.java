1d81d0f9c8748f211ad31efe1fc1de5e4af9d2e9
lindwnrl <lindwnrl@gmail.com>
2025-11-07 12:50:50 +0700

    Percobaan 4

---
+++ Jobsheet9/SearchNilai17.java
@@ -0,0 +1,18 @@
+public class SearchNilai17 {
+    public static void main(String[] args) {
+        int nilai[] ={80,85,78,96,90,82,86};
+        int key = 90;
+        int hasil = 0;
+        
+
+        for (int i = 0;i <nilai.length;i++){
+            if (key == nilai [i]){
+                hasil = i;
+                break;
+            }
+        }
+        System.out.println();
+        System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
+        System.out.println();
+    }
+}
