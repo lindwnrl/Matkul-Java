f48a48fcbd0493d46fabe6372f9b5744eaf2cc48
lindwnrl <lindwnrl@gmail.com>
2025-11-13 15:38:54 +0700

    Modifikasi Percobaan 1

--- Jobsheet10/Bioskop17.java
+++ Jobsheet10/Bioskop17.java
@@ -9,10 +9,27 @@ public class Bioskop17 {
         penonton [2][0] ="Nurus";
         penonton [2][1] ="Raden";
         penonton [3][0] ="Gw";
-
+        penonton [3][1] ="Kaped";
+        
         System.out.printf("%s \t %s \n",penonton [0][0],penonton[0][1]);
         System.out.printf("%s \t %s \n",penonton [1][0],penonton[1][1]);
         System.out.printf("%s \t %s \n",penonton [2][0],penonton[2][1]);
         System.out.printf("%s \t %s \n",penonton [3][0],penonton[3][1]);
+        
+        for (int i = 0;i < penonton.length ;i++){
+            System.out.println("Panjang baris ke-" + (i+1) +" :" +String.join(", ", penonton[i]));
+        }
+        System.out.println(penonton.length);
+        for (String [] barisPenonton : penonton){
+            System.out.println("Panjang baris : "+barisPenonton.length);
+        }
+        System.out.println("Penonton pada baris ke-3 : ");
+        for (int i = 0;i < penonton[2].length;i++){
+            System.out.println(penonton[2][i]);
+        }
+        System.out.println("Penonton pada baris ke-3 : ");
+         for (String i : penonton[2]){
+            System.out.println((i));
+        }
     }
 }
