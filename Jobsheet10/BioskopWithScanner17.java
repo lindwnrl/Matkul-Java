a4c9e600c2ee8b7a8e1d628012effcee51a5f585
lindwnrl <lindwnrl@gmail.com>
2025-11-15 07:20:30 +0700

    Percobaan 1

---
+++ Jobsheet10/BioskopWithScanner17.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+import javax.print.DocFlavor.STRING;
+import javax.swing.plaf.synth.SynthIcon;
+
+public class BioskopWithScanner17 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nama,next;
+        int baris,kolom;
+        String penonton [][] = new String[4][2];
+
+        while (true) {
+            System.out.print("Masukkan nama : ");
+            nama = sc.nextLine();
+            System.out.print("Masukkan baris : ");
+            baris =sc.nextInt();
+            System.out.print("Masukkan kolom : ");
+            kolom = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Apa ada lagi kak? : (y/n)");
+            next = sc.nextLine();
+
+            penonton [baris - 1][kolom - 1] = nama;
+            if (next.equalsIgnoreCase("n")) {
+                break;
+            }
+        }
+        System.out.println(penonton);
+        // System.out.println("Berikut adalah data penonton yang ada : ");
+        // for (int i = 0 i <)
+    }
+}
