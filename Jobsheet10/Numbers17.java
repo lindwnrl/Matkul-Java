4e9cdfc6db6608cf4801ce53e482c65834ee68e5
lindwnrl <lindwnrl@gmail.com>
2025-11-16 20:38:57 +0700

    Modifikasi Percobaan 3

--- Jobsheet10/Numbers17.java
+++ Jobsheet10/Numbers17.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Numbers17 {
@@ -7,5 +8,13 @@ public class Numbers17 {
         myNumbers [0] = new int[5];
         myNumbers [1] = new int[3];
         myNumbers [2] = new int[1];
+
+        for (int i = 0;i < myNumbers.length;i++){
+            System.out.println(Arrays.toString(myNumbers [i]));
+        }
+        for (int i = 0;i < myNumbers.length;i++){
+            System.out.println("Panjang baris ke-"+(i + 1)+" : " + myNumbers[i].length);
+        }
+        sc.close();
     }
 }
