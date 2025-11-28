54f4f8d28b66acda6402a16c01014aaa373d4d55
lindwnrl <lindwnrl@gmail.com>
2025-11-28 10:01:09 +0700

    Percobaan 2

--- Jobsheet10/Jobsheet11/Kafe17.java
+++ Jobsheet10/Jobsheet11/Kafe17.java
@@ -1,7 +1,11 @@
 package Jobsheet11;
 
 public class Kafe17 {
-    public static void Menu (){
+    public static void Menu (String namaPelanggan,boolean isMember){
+        System.out.println("Selamat datang "+ namaPelanggan + " !");
+        if (isMember) {
+            System.out.println("Anda adalah member,dapatkan diskon 10% untuk setiap pembelian!");
+        }
         System.out.println("=== Menu Resto Kafe ===");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -12,6 +16,6 @@ public class Kafe17 {
         System.out.println("========================");
     }
     public static void main(String[] args) {
-        Menu();
-    }
+        Menu("Andi",true);
 }
+    }
