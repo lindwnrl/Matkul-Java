74f1518eb5eb48a0c247d615e6b18983ba474539
lindwnrl <lindwnrl@gmail.com>
2025-12-01 05:12:02 +0700

    Modifikasi Percobaan 4

--- Jobsheet11/PengunjungCafe17.java
+++ Jobsheet11/PengunjungCafe17.java
@@ -3,13 +3,13 @@ package Jobsheet11;
 public class PengunjungCafe17 {
     static void daftarPengunjung(String... namaPengunjung){
         System.out.println("Daftar Nama Pengunjung");
-        for (int i = 0; i < namaPengunjung.length;i++){
-            System.out.println("- "+ namaPengunjung[i]);
+        for (String nama : namaPengunjung){
+            System.out.println("- "+ nama);
         }
     }
     public static void main(String[] args) {
-        daftarPengunjung("Ali","Budi","Citra","Biji");
-        daftarPengunjung("Andi");
-        daftarPengunjung("Doni","Eti","Fahmi","Galih");
+        daftarPengunjung();
+        daftarPengunjung();
+        daftarPengunjung();
     }
 }
