10c5e1ad211b9b904fdf4e394cc72fb86780c552
lindwnrl <lindwnrl@gmail.com>
2025-11-30 19:16:10 +0700

    Percobaan 3

--- Jobsheet10/Jobsheet11/Kafe17.java
+++ Jobsheet10/Jobsheet11/Kafe17.java
@@ -1,7 +1,10 @@
 package Jobsheet11;
 
+import java.util.Scanner;
+
 public class Kafe17 {
     public static void Menu (String namaPelanggan,boolean isMember,String kodePromo){
+        
         System.out.println("Selamat datang "+ namaPelanggan + " !");
         if (isMember) {
             System.out.println("Anda adalah member,dapatkan diskon 10% untuk setiap pembelian!");
@@ -21,8 +24,24 @@ public class Kafe17 {
         System.out.println("5. Roti Bakar - Rp 10,000");
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("========================");
+        
+    }
+    public static int hitungTotalHarga17(int pilihanMenu,int banyakItem){
+        int [] hargaItems = {15000,20000,22000,12000,10000,18000};
+
+        int hargaTotal = hargaItems [pilihanMenu - 1] *banyakItem;
+        return hargaTotal;
     }
     public static void main(String[] args) {
         Menu("Budi",true,"DISKON30");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nomor menu yang ingin anda pesan :");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan :");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga17(pilihanMenu, banyakItem);
+
+        System.out.println("Total harga untuk pesanan anda : " +totalHarga);
 }
     }
