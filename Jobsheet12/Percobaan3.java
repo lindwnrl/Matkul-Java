aaad8e542613dc1e6fa9c5badf319e6386195bee
lindwnrl <lindwnrl@gmail.com>
2025-12-06 20:39:05 +0700

    Percobaan 3

---
+++ Jobsheet12/Percobaan3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Percobaan3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan Saldo awal : ");
+        double saldo =sc.nextInt();
+        System.out.print("Masukkan lamanya investasi : ");
+        int tahun = sc.nextInt();
+        System.out.print("Jumlah saldo setelah "+tahun+" tahun adalah : ");
+        System.out.print(hitungLaba(saldo, tahun));
+        sc.close();
+    }
+    static double hitungLaba(double saldo,int tahun){
+        
+        if (tahun == 0) {
+            return (saldo);
+        }else {
+            return (1.11 * hitungLaba(saldo, tahun - 1));
+        }
+
+    }
+}
